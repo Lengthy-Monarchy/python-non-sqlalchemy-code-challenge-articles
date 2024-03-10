4e851fc3726b8f4de4abca53535a6bddd7012aec
annei <anneirungu07@gmail.com>
2024-03-10 11:32:50 +0300

    first commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -3,36 +3,63 @@ class Article:
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if isinstance(value, str) and 5 <= len(value) <= 50:
+            self._title = value
+        else:
+            raise ValueError("Title must be a string between 5 and 50 characters")
+
 class Author:
     def __init__(self, name):
         self.name = name
+        self._articles = []
 
+    @property
+    def name(self):
+        return self._name
+
+    @property
     def articles(self):
-        pass
-
-    def magazines(self):
-        pass
+        return self._articles
 
     def add_article(self, magazine, title):
-        pass
-
-    def topic_areas(self):
-        pass
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        return article
 
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self._articles = []
 
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def category(self):
+        return self._category
+
+    @property
     def articles(self):
-        pass
+        return self._articles
 
-    def contributors(self):
-        pass
-
-    def article_titles(self):
-        pass
-
-    def contributing_authors(self):
-        pass
+    def add_article(self, author, title):
+        article = Article(author, self, title)
+        self._articles.append(article)
+        return article
